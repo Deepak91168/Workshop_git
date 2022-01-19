cbb3db33819e7fd898a3639fbac614efb9a6a7cd
Deepak <20bcs102@nith.ac.in>
2022-01-19 12:09:56 +0530

    Updated Readme.md and Added Deepak_20bcs102.cpp

---
+++ TEST/Deepak_20bcs102.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<"GIT PRACTICE"<<endl;
+    return 0;
+}
